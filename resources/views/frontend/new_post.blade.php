a98d8c6c10d91f4a2426e61c8fd9d10adabe20e1
manh <manhmeo1117@gmail.com>
2022-05-06 10:10:45 +0700

    manh

--- resources/views/frontend/new_post.blade.php
+++ resources/views/frontend/new_post.blade.php
@@ -56,32 +56,16 @@
                          Video
                         </h3>
                     </div>
-                        <div class="p-tb-16 p-rl-25 ">
-                            <iframe  src="https://www.youtube.com/embed/6t-MjBazs3o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </div>
+                            <img class="max-w-full" src="/images/dmx.gif" alt="IMG">
+                    </div>
+                </div>            <div class="p-l-10 p-rl-0-sr991 p-b-20">
+                <!-- Video -->
+                <div class="p-b-55">
 
+                            <img class="max-w-full" src="/images/oto.gif" alt="IMG">
                     </div>
                 </div>
-
                 <!-- Subscribe -->
-                <div class="bg10 p-rl-35 p-t-28 p-b-35 m-b-55">
-                    <h5 class="f1-m-5 cl0 p-b-10">
-                        Subscribe
-                    </h5>
-
-                    <p class="f1-s-1 cl0 p-b-25">
-                        Get all latest content delivered to your email a few times a month.
-                    </p>
-
-                    <form class="size-a-9 pos-relative">
-                        <input class="s-full f1-m-6 cl6 plh9 p-l-20 p-r-55" type="text" name="email"
-                               placeholder="Email">
-
-                        <button class="size-a-10 flex-c-c ab-t-r fs-16 cl9 hov-cl10 trans-03">
-                            <i class="fa fa-arrow-right"></i>
-                        </button>
-                    </form>
-                </div>
             </div>
         </div>
     </div>
